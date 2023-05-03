3a098e331f1e1e9a2838ffae571d9c9115ff7b2e
Dima Roma <vazzda@gmail.com>
2023-05-03 05:51:20 +0200

    movement refactor: decomposition, no finite states

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-enum PlayerStates {MOVE, SWORD}
-var CurrentState = PlayerStates.MOVE
 
 var SPEED = 200.0
 var VELOCITY_CLAMNP = 100.0
@@ -9,76 +7,108 @@ var GRAVITY = 20
 var JUMP = 400
 var JUMP_IN_AIR_LIMIT = 2
 
+enum PlayerStates {MOVE, SWORD}
+var CurrentState = PlayerStates.MOVE
 var jump_in_air_counter = 0
+var swordAnimInProgress = false
 
 func _ready():
 	$Sword/CollisionShape2D.disabled = true
 	$anim.connect("animation_finished", _onAnimationFinised)
 
 func _physics_process(delta):
-#	Move(delta)
-	
-	match CurrentState:
-		PlayerStates.MOVE:
-			Move(delta)
-		PlayerStates.SWORD:
-			Sword()
-	
-	velocity.y += GRAVITY
+	_handleInputs(delta)
+	_handleGravity()
+	_updateAnims()
 	move_and_slide()
 
 
-func Move(delta):
+func _handleInputs(delta):
 	var movement = Input.get_action_strength("ui_move_right") - \
-			Input.get_action_strength("ui_move_left")
+		Input.get_action_strength("ui_move_left")
 
 	if movement != 0:
-		if movement > 0.0:
-			velocity.x += SPEED * delta
-			velocity.x = clamp(SPEED, VELOCITY_CLAMNP, SPEED)
-			$Sprite2D.flip_h = false
-			$anim.play("Walk")
-			$Sword/CollisionShape2D.position = Vector2(0, 0)
+		if movement > 0.0:		
+			_moveRight(delta)
 		if movement < 0.0:
-			velocity.x -= SPEED * delta
-			velocity.x = clamp(SPEED, -VELOCITY_CLAMNP, -SPEED)
-			$Sprite2D.flip_h = true
-			$anim.play("Walk")
-			$Sword/CollisionShape2D.position = Vector2(-50, 0)
-	
+			_moveLeft(delta)
 	if movement == 0.0:
-		velocity.x = 0.0
-		$anim.play("Idle")
+		_moveIdle()
+		
+	if Input.is_action_just_pressed("ui_jump"):
+		if is_on_floor():
+			_jump()
+		else:
+			_jumpInAir()
+	_checkJumpInAirState()
 	
-	if Input.is_action_just_pressed("ui_jump") && is_on_floor():
-		Jump()
-	if Input.is_action_just_pressed("ui_jump") && !is_on_floor():
-		if jump_in_air_counter < JUMP_IN_AIR_LIMIT:
-			Jump()
-			jump_in_air_counter += 1
+	if Input.is_action_just_pressed("ui_sword"):
+		_sword(movement)
+
+
+func _moveRight(delta):
+	velocity.x += SPEED * delta
+	velocity.x = clamp(SPEED, VELOCITY_CLAMNP, SPEED)
+	$Sprite2D.flip_h = false
+	$Sword/CollisionShape2D.position = Vector2(0, 0)
+
+func _moveLeft(delta):
+	velocity.x -= SPEED * delta
+	velocity.x = clamp(SPEED, -VELOCITY_CLAMNP, -SPEED)
+	$Sprite2D.flip_h = true
+	$Sword/CollisionShape2D.position = Vector2(-50, 0)
+
+func _moveIdle():
+	velocity.x = 0.0
+
+func _jump():
+	velocity.y = -JUMP
+	
+func _jumpInAir():
+	if jump_in_air_counter < JUMP_IN_AIR_LIMIT:
+		jump_in_air_counter += 1
+		_jump()
+
+func _checkJumpInAirState():
 	if is_on_floor() && jump_in_air_counter != 0:
 		jump_in_air_counter = 0
 
-	if !is_on_floor():
-		$anim.play("Jump")
+func _sword(movement):
+	CurrentState = PlayerStates.SWORD
+
+func _handleGravity():
+	velocity.y += GRAVITY
+
+
+func _updateAnims():
+	var movement = Input.get_action_strength("ui_move_right") - \
+		Input.get_action_strength("ui_move_left")
+	var currentAnim = $anim.get_current_animation()
+	
+	if CurrentState == PlayerStates.SWORD:
+		if currentAnim == "Sword":
+			return
+		else:
+			$anim.play("Sword")
+			return
+
 	if !is_on_floor() && velocity.y > 10:
 		$anim.play("Fall")
-	
-	if Input.is_action_just_pressed("ui_sword"):
-		CurrentState = PlayerStates.SWORD
-		velocity.x = movement
+		return
+	if !is_on_floor():
+		$anim.play("Jump")
+		return
 
-
-func Jump():
-	velocity.y = -JUMP
-
-func Sword():
-	$anim.play("Sword")
+	if movement != 0:
+		$anim.play("Walk")
+	else:
+		$anim.play("Idle")
 
 
 func _onAnimationFinised(name: String):
 	if name == "Sword":
 		_resetPlayerState()
+
 
 func _resetPlayerState():
 	CurrentState = PlayerStates.MOVE
