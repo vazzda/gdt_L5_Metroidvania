8ae8be9e65c520d65e45b85b8a7a2d177e2ac95a
Dima Roma <vazzda@gmail.com>
2023-05-07 21:22:16 +0200

    small GUI code reafactor

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -3,30 +3,27 @@ extends CanvasLayer
 const HEART_ROW_SIZE = 8
 const HEART_OFFSET = 16
 
+@onready var HEART = $Heart
+@onready var COINS_LABEL = $coinText
+
 var GUI_healh_cup
 var GUI_healh_current
 
 
 func _ready():
-	for i in Globals.playerLives:
-		var new_heart = Sprite2D.new()
-		new_heart.texture = $Heart.texture
-		new_heart.hframes = $Heart.hframes
-		$Heart.add_child(new_heart)
-	
+	_setHealthUI()
 	Globals.connect("COIN_CHANGED", updateCoins)
 	resetHUD()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_checkForLivesSync()
+
 
 func resetHUD():
 	updateCoins()
 	
 func redrawHeatlh():
-	for heart in $Heart.get_children():
+	for heart in HEART.get_children():
 		var index = heart.get_index()
 		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET
 		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
@@ -38,6 +35,10 @@ func redrawHeatlh():
 		if index < last_heart:
 			heart.frame = 4
 
+func updateCoins():
+	COINS_LABEL.text = str(Globals.getCoins())
+
+
 func _checkForLivesSync():
 	if Globals.playerLives != GUI_healh_current || \
 		Globals.playerLivesCap != GUI_healh_cup:
@@ -45,7 +46,9 @@ func _checkForLivesSync():
 	GUI_healh_current = Globals.playerLives
 	GUI_healh_cup = Globals.playerLivesCap
 
-
-func updateCoins():
-	print(123)
-	$coinText.text = str(Globals.getCoins())
+func _setHealthUI():
+	for i in Globals.playerLives:
+		var new_heart = Sprite2D.new()
+		new_heart.texture = HEART.texture
+		new_heart.hframes = HEART.hframes
+		HEART.add_child(new_heart)
